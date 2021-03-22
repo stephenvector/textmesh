d95a4cb465296028a21d803ecb6f65e4991d77e2
Stephen Vector <55390907+stephenvector@users.noreply.github.com>
2021-03-21 23:27:28 -0700

    adding cancel & save button components

---
+++ src/CancelButton.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+import SaveButton from "./SaveButton";
+
+const CancelButton = styled(SaveButton)`
+  background: #f2f2f2;
+  color: #000;
+`;
+
+export default CancelButton;
---
+++ src/SaveButton.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const SaveButton = styled.button`
+  background: #5d8;
+  color: #fff;
+  border: none;
+  font: inherit;
+  padding: 0.5rem 1rem;
+  border-radius: 0;
+`;
+
+export default SaveButton;
